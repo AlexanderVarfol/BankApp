fdf5a115529d56f7d62c1e4fa013f86b8160cbeb
Alexander <varfolomeevalexandr3@gmail.com>
2023-11-30 20:48:44 +0100

    status .30_11. commit

---
+++ src/main/java/com/bankservice/app/dto/AgreementDTO.java
@@ -0,0 +1,22 @@
+package com.bankservice.app.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class AgreementDTO {
+
+    //Agreement
+    private String id;
+    private String sum;
+    private String interestRate;
+
+    //Account
+    private
+
+}
